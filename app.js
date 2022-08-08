34d29f11575e73ee8128b764b810724bc7a24ec2
ddidee <olawaleadeola20@gmail.com>
2022-08-08 12:39:57 +0100

    Node js Intro

---
+++ app.js
@@ -0,0 +1,19 @@
+// console.log('sail labs ikorodu');
+
+// var sail = {
+//     lounge1: 'segun',
+//     lounge2: 'baby deola',
+//     lounge3: 'sola'
+
+// }
+
+// console.log(sail);
+
+function multiply(a, b) {
+    return a*b
+}
+
+// let multiply = (a, b) => a*b;
+
+
+console.log(multiply (7, 7));
